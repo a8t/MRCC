c2b0578fcaaa6425442ba2bb4cd9efeef5a60494
Andy Tran <andy8tran@gmail.com>
2019-06-12 09:05:27 -0400

    Fix index page preview

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,6 +6,108 @@ import Layout from '../components/Layout';
 import Features from '../components/Features';
 import BlogRoll from '../components/BlogRoll';
 
+export const IndexPageTemplate = ({
+  image,
+  title,
+  subtitle,
+  mainpitch,
+  intro,
+}) => (
+  <React.Fragment>
+    <header
+      className="full-width-image margin-top-0"
+      style={{
+        backgroundImage: `url(${
+          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+        })`,
+        backgroundPosition: `bottom`,
+
+        // backgroundPosition: `top center`,
+        // backgroundAttachment: 'fixed',
+        // backgroundSize: 'auto',
+        // backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          height: '150px',
+          lineHeight: '1',
+          justifyContent: 'space-around',
+          alignItems: 'left',
+          flexDirection: 'column',
+        }}
+      >
+        <h1
+          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+          style={{
+            backgroundColor: 'rgba(0, 68, 68, 0.7)',
+            color: 'white',
+            lineHeight: '1',
+            padding: '0.25em',
+          }}
+        >
+          {title}
+        </h1>
+        <h3
+          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+          style={{
+            backgroundColor: 'rgba(0, 68, 68, 0.7)',
+            color: 'white',
+            lineHeight: '1',
+            padding: '0.25em',
+          }}
+        >
+          {subtitle}
+        </h3>
+      </div>
+    </header>
+    <main style={{ display: 'flex', flexDirection: 'column' }}>
+      <section
+        style={{
+          width: '100%',
+          maxWidth: '60rem',
+          margin: 'auto',
+          padding: 16,
+        }}
+      >
+        <h1 className="title">{mainpitch.title}</h1>
+        <h5 className="subtitle">{mainpitch.description}</h5>
+      </section>
+      <section
+        style={{
+          width: '100%',
+          maxWidth: '60rem',
+          margin: 'auto',
+          padding: 16,
+        }}
+      >
+        <h1 className="title">{intro.heading}</h1>
+        <h5 className="subtitle">{intro.description}</h5>
+
+        <Features gridItems={intro.blurbs} />
+      </section>
+      <section
+        style={{
+          width: '100%',
+          maxWidth: '60rem',
+          margin: 'auto',
+          padding: 16,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        <h1 className="title">Latest stories</h1>
+        <BlogRoll />
+        <Link className="btn" to="/blog" style={{ alignSelf: 'center' }}>
+          Read more
+        </Link>
+      </section>
+    </main>
+  </React.Fragment>
+);
+
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark;
   const {
@@ -20,76 +122,15 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
-      <header
-        className="full-width-image margin-top-0"
-        style={{
-          backgroundImage: `url(${
-            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-          })`,
-          backgroundPosition: `bottom`,
-
-          // backgroundPosition: `top center`,
-          // backgroundAttachment: 'fixed',
-          // backgroundSize: 'auto',
-          // backgroundRepeat: 'no-repeat',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            height: '150px',
-            lineHeight: '1',
-            justifyContent: 'space-around',
-            alignItems: 'left',
-            flexDirection: 'column',
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-            style={{
-              backgroundColor: 'rgba(0, 68, 68, 0.7)',
-              color: 'white',
-              lineHeight: '1',
-              padding: '0.25em',
-            }}
-          >
-            {title}
-          </h1>
-          <h3
-            className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-            style={{
-              backgroundColor: 'rgba(0, 68, 68, 0.7)',
-              color: 'white',
-              lineHeight: '1',
-              padding: '0.25em',
-            }}
-          >
-            {subtitle}
-          </h3>
-        </div>
-      </header>
-      <main style={{ display: 'flex', flexDirection: 'column' }}>
-        <section style={{ maxWidth: '60rem', margin: 'auto', padding: 16 }}>
-          <h1 className="title">{mainpitch.title}</h1>
-          <h5 className="subtitle">{mainpitch.description}</h5>
-        </section>
-        <section style={{ maxWidth: '60rem', margin: 'auto', padding: 16 }}>
-          <h1 className="title">{intro.heading}</h1>
-          <h5 className="subtitle">{intro.description}</h5>
-
-          <Features gridItems={intro.blurbs} />
-
-          {/* <div className="column is-12">
-          <h3 className="has-text-weight-semibold is-size-2">Latest stories</h3>
-          <BlogRoll />
-          <div className="column is-12 has-text-centered">
-            <Link className="btn" to="/blog">
-              Read more
-            </Link>
-          </div>
-        </div> */}
-        </section>
-      </main>
+      <IndexPageTemplate
+        image={image}
+        title={title}
+        heading={heading}
+        subtitle={subtitle}
+        description={description}
+        intro={intro || { blurbs: [] }}
+        mainpitch={mainpitch || {}}
+      />
     </Layout>
   );
 };
