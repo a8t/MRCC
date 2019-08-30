46e5b0613087771506932f89dba36557b36e69e1
Andy Tran <andy8tran@gmail.com>
2019-08-30 17:53:25 -0400

    Switch Navbar to hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,82 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import logo from '../img/mrc_logo_only.svg';
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    };
-  }
+const Navbar = () => {
+  const [active, setActive] = useState(false);
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            });
-      }
-    );
+  const toggleHamburger = () => {
+    setActive(!active);
   };
 
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent is-fixed-top"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="MRCC" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
+  const navBarActiveClass = active ? 'is-active' : '';
+
+  return (
+    <nav
+      className="navbar is-transparent is-fixed-top"
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item" title="Logo">
+            <img src={logo} alt="MRCC" style={{ width: '88px' }} />
+          </Link>
+          {/* Hamburger menu */}
           <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+            className={`navbar-burger burger ${navBarActiveClass}`}
+            data-target="navMenu"
+            onClick={() => toggleHamburger()}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/mission">
-                Our mission
-              </Link>
-              <Link className="navbar-item" to="/">
-                Campaigns
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-            </div>
+            <span />
+            <span />
+            <span />
           </div>
         </div>
-      </nav>
-    );
-  }
+        <div id="navMenu" className={`navbar-menu ${navBarActiveClass}`}>
+          <div className="navbar-start has-text-centered">
+            <Link className="navbar-item" to="/mission">
+              Our mission
+            </Link>
+            <Link className="navbar-item" to="/">
+              Campaigns
+            </Link>
+            <Link className="navbar-item" to="/blog">
+              Blog
+            </Link>
+            <Link className="navbar-item" to="/contact">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
