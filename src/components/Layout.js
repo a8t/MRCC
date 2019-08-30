ca542bd61975d639f8052649c16c909c1a821863
Andy Tran <andy8tran@gmail.com>
2019-08-30 16:52:48 -0400

    Provide theme

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,54 +1,58 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { ThemeProvider } from 'styled-components';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import theme from '../components/theme';
 import './all.sass';
 import useSiteMetadata from './SiteMetadata';
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata();
   return (
-    <React.Fragment>
-      <Helmet>
-        <html lang="en" className="has-navbar-fixed-top" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <Helmet>
+          <html lang="en" className="has-navbar-fixed-top" />
+          <title>{title}</title>
+          <meta name="description" content={description} />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/apple-touch-icon.png"
-        />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/apple-touch-icon.png"
+          />
 
-        <link
-          rel="icon"
-          type="image/svg"
-          href="/img/favicon.ico"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/svg"
-          href="/img/favicon.ico"
-          sizes="16x16"
-        />
+          <link
+            rel="icon"
+            type="image/svg"
+            href="/img/favicon.ico"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/svg"
+            href="/img/favicon.ico"
+            sizes="16x16"
+          />
 
-        <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
+          <link
+            rel="mask-icon"
+            href="/img/safari-pinned-tab.svg"
+            color="#ff4400"
+          />
+          <meta name="theme-color" content="#fff" />
 
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta property="og:image" content="/img/og-image.jpg" />
-      </Helmet>
-      <Navbar />
-      {children}
-      <Footer />
-    </React.Fragment>
+          <meta property="og:type" content="business.business" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content="/" />
+          <meta property="og:image" content="/img/og-image.jpg" />
+        </Helmet>
+        <Navbar />
+        {children}
+        <Footer />
+      </React.Fragment>
+    </ThemeProvider>
   );
 };
 
