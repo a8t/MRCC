c04902e0d172165850a26b1370041a538e9a712a
Andy Tran <andy8tran@gmail.com>
2019-08-30 16:52:35 -0400

    [blog] redesign blog landing page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,30 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../../components/Layout';
 import BlogRoll from '../../components/BlogRoll';
+
+const Header = styled.div`
+  width: 100vw;
+  height: 400px;
+  background-size: cover;
+  background-position: bottom;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: white;
+    padding: 1rem;
+  }
+`;
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
+        <Header
           style={{
             backgroundImage: `url('/img/mrc_01.jpg')`,
           }}
         >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
+          <h1 className="has-text-weight-bold is-size-1 has-primary-background">
             Latest Stories
           </h1>
-        </div>
+        </Header>
         <section className="section">
           <div className="container">
             <div className="content">
