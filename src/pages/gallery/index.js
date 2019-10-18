96c90fcba14c922cc04d3bf78edaf6efcf338278
Andy Tran <andy8tran@gmail.com>
2019-10-18 14:41:09 -0400

    Add index page for gallery

---
+++ src/pages/gallery/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import Layout from '../../components/shared/Layout';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const query = graphql`
+  query GalleryIndexQuery {
+    allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "gallery-page" } } }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            gallery
+            title
+            subtitle
+            mainPhoto {
+              childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const GalleriesMain = styled.main`
+  max-width: 60em;
+  margin: auto;
+  padding: 8vmin;
+
+  .galleries {
+    margin: 0;
+    margin-top: 32px;
+    padding: 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 225px);
+    list-style: none;
+    li {
+      display: block;
+    }
+  }
+`;
+
+export default function GalleryPage() {
+  const {
+    allMarkdownRemark: { edges },
+  } = useStaticQuery(query);
+
+  return (
+    <Layout>
+      <GalleriesMain
+        className="content"
+        style={{ maxWidth: '60em', margin: 'auto', padding: '8vmin' }}
+      >
+        <h1>Galleries</h1>
+        <ul className="galleries">
+          {edges.map(
+            ({
+              node: {
+                frontmatter: { gallery, title, subtitle, mainPhoto },
+              },
+            }) => {
+              return (
+                <li className="card">
+                  <Link to={`/gallery/${gallery}`}>
+                    <Img
+                      className="card-image"
+                      fluid={mainPhoto.childImageSharp.fluid}
+                    />
+                    <div className="card-content">
+                      <p class="title is-4">{title}</p>
+                      <p class="subtitle is-6">{subtitle}</p>
+                    </div>
+                  </Link>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </GalleriesMain>
+    </Layout>
+  );
+}
