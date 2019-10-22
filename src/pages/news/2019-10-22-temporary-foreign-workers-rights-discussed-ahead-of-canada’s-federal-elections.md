11a70e7e45228a7cfe64fbe4d5456b582c6815be
Andy Tran <andy8tran@gmail.com>
2019-10-22 17:15:13 -0400

    Update news post style

--- src/pages/news/2019-10-22-temporary-foreign-workers-rights-discussed-ahead-of-canada’s-federal-elections.md
+++ src/pages/news/2019-10-22-temporary-foreign-workers-rights-discussed-ahead-of-canada’s-federal-elections.md
@@ -1,12 +1,16 @@
 ---
 templateKey: news-post
-title: TEMPORARY FOREIGN WORKERS RIGHTS DISCUSSED AHEAD OF CANADA’S FEDERAL ELECTIONS
+title: Temporary Foreign Workers’ Rights Discussed Ahead of Canada’s Federal Elections
 date: 2019-09-11T00:55:14.663Z
 description: Re post from Balitang America. Published by Christine Santos
 featuredpost: true
 featuredimage: /img/72333254_2680940595291633_68978854105972736_o.jpg
+location: Toronto
+tags:
+  - impact
 ---
-TORONTO —  “We just want to work here…we just want to help our family back home!”
+
+> “We just want to work here… we just want to help our family back home!”
 
 This Filipina temporary foreign worker represents the plight of many who are reportedly exploited by unscrupulous employers.
 
@@ -14,7 +18,7 @@ These low-skilled workers move from the Philippines to Canada, hoping for better
 
 To help educate migrant workers of their rights, the Migrant Resource Center recently facilitated a workshop on federal immigration policies and provincial labor laws.
 
-“Ang buong akala namin is wala kaming karapatan.  Natatakot kami lagi na ma-fired, mawalan ng trabaho at mapauwi ng Pilipinas. Naintindihan ko na na bilang worker, may karapatan akong magsailta…na may karapatan ako na ipaglaban kung ano yung rights ko.“
+“Ang buong akala namin is wala kaming karapatan. Natatakot kami lagi na ma-fired, mawalan ng trabaho at mapauwi ng Pilipinas. Naintindihan ko na na bilang worker, may karapatan akong magsailta…na may karapatan ako na ipaglaban kung ano yung rights ko.“
 
 “We didn’t think we had rights. We were always scared to lose our jobs and that we had to go back to the Philippines. Now I understand, that as a worker, I have the right to speak up and fight for those rights.”
 
@@ -24,8 +28,8 @@ While the Liberal government plans to continue welcoming temporary foreign worke
 
 “First priority is to give jobs to Canadians, but in some sectors of our economy, there is not enough workers domestically who are available so there’s a need to attract foreign workers,” said Ahmed Hussen, minister of immigration.
 
-“We presented in February 2019 a comprehensive strategy to deal with unauthorized immigration consultants — $52.9 million dollar strategy when we went in government to tackle that issue.”
+“We presented in February 2019 a comprehensive strategy to deal with unauthorized immigration consultants — \$52.9 million dollar strategy when we went in government to tackle that issue.”
 
-“There’s no strong mechanism to punish illegal recruiters and this is why it continues to happen.  We are campaigning for the Ministry of Labour in Ontario to provide licensing for all private recruitment agencies, register all employers who are hiring temporary foreign workers and lastly, permanent residency upon arrival for all migrant workers,” said Jesson Reyes, Managing the Director of Migrants Resource Center Canada.
+“There’s no strong mechanism to punish illegal recruiters and this is why it continues to happen. We are campaigning for the Ministry of Labour in Ontario to provide licensing for all private recruitment agencies, register all employers who are hiring temporary foreign workers and lastly, permanent residency upon arrival for all migrant workers,” said Jesson Reyes, Managing the Director of Migrants Resource Center Canada.
 
 More workshops are offered by the MRCC over the coming months to promote the rights and dignity of migrant and immigrant workers in Canada.
--- src/templates/news-post.js
+++ src/templates/news-post.js
@@ -3,46 +3,123 @@ import PropTypes from 'prop-types';
 import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
 import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 import Layout from '../components/shared/Layout';
 import Content, { HTMLContent } from '../components/shared/Content';
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  header {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: 'both-items';
+
+    .splash {
+      grid-area: both-items;
+      height: 30vh;
+      object-fit: cover;
+      object-position: 50% 20%;
+    }
+
+    @media screen and (min-width: 1024px) {
+      .splash {
+        height: 50vh;
+        width: 100%;
+      }
+    }
+
+    .splash {
+      z-index: -1;
+    }
+  }
+
+  .card {
+    padding: 8vmin;
+    margin: 2vmin;
+    margin-top: -10vh;
+    margin-bottom: 16px;
+    max-width: 50em;
+
+    @media (min-width: 1024px) {
+      box-shadow: 0 17px 50px 0 rgba(0, 0, 0, 0.19),
+        0 12px 15px 0 rgba(0, 0, 0, 0.24);
+      margin: auto;
+      margin-top: -30vh;
+      margin-bottom: 64px;
+    }
+
+    display: flex;
+    flex-direction: column;
+
+    .news-post__title {
+      line-height: 1.1em;
+    }
+
+    .news-post__date {
+      margin-left: auto;
+      margin-top: 16px;
+      margin-bottom: 32px;
+      font-style: italic;
+    }
+  }
+`;
 
 export const NewsPostTemplate = ({
   content,
   contentComponent,
-  description,
-  tags,
-  title,
+  frontMatter,
   helmet,
 }) => {
   const PostContent = contentComponent || Content;
 
+  const {
+    featuredImage,
+    title,
+    date,
+    description,
+    featuredpost,
+    featuredimage,
+    location,
+    tags,
+  } = frontMatter;
+
   return (
-    <section className="section">
+    <Main>
       {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+      <header>
+        <Img className="splash" fluid={featuredimage.childImageSharp.fluid} />
+      </header>
+      <section className="card">
+        <h1 className="news-post__title has-text-weight-bold is-size-5-mobile is-size-3-tablet is-size-1-desktop ">
+          {title}
+        </h1>
+
+        <span className="news-post__date">
+          {[location, date].filter(Boolean).join('  •  ')}
+        </span>
+
+        <PostContent className="content" content={content} />
+
+        {tags && tags.length ? (
+          <div style={{ marginTop: `4rem` }}>
+            <h4>Tags</h4>
+            <ul className="taglist">
+              {tags.map(tag => (
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
-      </div>
-    </section>
+        ) : null}
+      </section>
+    </Main>
   );
 };
 
@@ -62,7 +139,7 @@ const NewsPost = ({ data }) => {
       <NewsPostTemplate
         content={post.html}
         contentComponent={HTMLContent}
-        description={post.frontmatter.description}
+        frontMatter={post.frontmatter}
         helmet={
           <Helmet titleTemplate="%s | News">
             <title>{`${post.frontmatter.title}`}</title>
@@ -72,8 +149,6 @@ const NewsPost = ({ data }) => {
             />
           </Helmet>
         }
-        tags={post.frontmatter.tags}
-        title={post.frontmatter.title}
       />
     </Layout>
   );
@@ -97,6 +172,14 @@ export const pageQuery = graphql`
         title
         description
         tags
+        location
+        featuredimage {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
