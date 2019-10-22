3e377a72096d3fe31c69a6e7a94235871c4e6700
Andy Tran <andy8tran@gmail.com>
2019-10-22 16:09:24 -0400

    Fix imports

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -3,10 +3,10 @@ import { Link } from 'gatsby';
 
 import Logo from './Logo';
 
-import facebook from '../../img/social/facebook.svg';
-import instagram from '../../img/social/instagram.svg';
-import twitter from '../../img/social/twitter.svg';
-import vimeo from '../../img/social/vimeo.svg';
+// import facebook from '../../img/social/facebook.svg';
+// import instagram from '../../img/social/instagram.svg';
+// import twitter from '../../img/social/twitter.svg';
+// import vimeo from '../../img/social/vimeo.svg';
 
 const Footer = class extends React.Component {
   render() {
