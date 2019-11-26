d9089454475170c287a96e4a5235b40ccbda5ebe
Andy Tran <andy8tran@gmail.com>
2019-11-26 14:08:09 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,12 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-150754711-1',
+      },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
